6941322696aa6a86040f931c82e4978ceee900a6
NahuelAlaniz <nahuelalaniz80@gmail.com>
2025-01-19 16:24:44 -0300

    Uso *ngFor para enlistar objetos en el componente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,7 @@ import {Housinglocation} from '../housinglocation';
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location *ngFor="let housingLocation of housingLocationList"[housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
@@ -21,12 +21,142 @@ import {Housinglocation} from '../housinglocation';
 export class HomeComponent {
   readonly baseUrl= 'https://angular.dev/assets/images/tutorials/common';
 
-  housingLocation: Housinglocation= {
-    id: 9999,
-    name: 'Black Cofee',
-    photo: `assets/images/black_cofee.jpg`,
-    availableUnits: 99,
-    place: 'Test place',
-    state: 'ST'
-  };
+  housingLocationList: Housinglocation[]= [
+    {
+      id: 0,
+      name: 'Expresso',
+      photo: `assets/images/expresso.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 2,
+      name: 'Ristretto',
+      photo: `assets/images/ristretto.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 3,
+      name: 'Cortado or macchiato',
+      photo: `assets/images/cortado.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 4,
+      name: 'Americano',
+      photo: `assets/images/americano.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 5,
+      name: 'Lungo/large',
+      photo: `assets/images/lungo.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 6,
+      name: 'Carajillo',
+      photo: `assets/images/carajillo.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 7,
+      name: 'Coffee with milk',
+      photo: `assets/images/cofee_with_milk.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 8,
+      name: 'Chocolate coffee',
+      photo: `assets/images/chocolate_coffee.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 9,
+      name: 'Capuchino',
+      photo: `assets/images/capuchino.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 10,
+      name: 'Mocca',
+      photo: `assets/images/mocca.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 11,
+      name: 'Irish coffee',
+      photo: `assets/images/irish_coffee.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 12,
+      name: 'Viennese coffee',
+      photo: `assets/images/viennese_coffee.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 13,
+      name: 'Frappé',
+      photo: `assets/images/frappe.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 14,
+      name: 'Azteca',
+      photo: `assets/images/azteca.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 15,
+      name: 'Arabica coffee',
+      photo: `assets/images/arabica.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 16,
+      name: 'Robusta coffee',
+      photo: `assets/images/robusta.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+    {
+      id: 17,
+      name: 'Combinations',
+      photo: `assets/images/combinations.jpg`,
+      availableUnits: 99,
+      place: 'Test place',
+      state: 'ST'
+    },
+  ];
 }
