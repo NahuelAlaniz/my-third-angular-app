5674f4c18fb9b60b91d8617ddc8de349f95f2c88
NahuelAlaniz <nahuelalaniz80@gmail.com>
2025-01-28 14:29:38 -0300

    Servicios Angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
-import {Housinglocation} from '../housinglocation';
+import {HousingLocation} from '../housinglocation';
+import { HousingService } from '../housing.service';
 @Component({
   selector: 'app-home',
   imports: [CommonModule, HousingLocationComponent],
@@ -19,144 +20,10 @@ import {Housinglocation} from '../housinglocation';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  readonly baseUrl= 'https://angular.dev/assets/images/tutorials/common';
+  housingLocationList: HousingLocation[]= [];
+  housingService: HousingService = inject(HousingService);
 
-  housingLocationList: Housinglocation[]= [
-    {
-      id: 0,
-      name: 'Expresso',
-      photo: `assets/images/expresso.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 2,
-      name: 'Ristretto',
-      photo: `assets/images/ristretto.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 3,
-      name: 'Cortado or macchiato',
-      photo: `assets/images/cortado.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 4,
-      name: 'Americano',
-      photo: `assets/images/americano.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 5,
-      name: 'Lungo/large',
-      photo: `assets/images/lungo.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 6,
-      name: 'Carajillo',
-      photo: `assets/images/carajillo.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 7,
-      name: 'Coffee with milk',
-      photo: `assets/images/cofee_with_milk.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 8,
-      name: 'Chocolate coffee',
-      photo: `assets/images/chocolate_coffee.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 9,
-      name: 'Capuchino',
-      photo: `assets/images/capuchino.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 10,
-      name: 'Mocca',
-      photo: `assets/images/mocca.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 11,
-      name: 'Irish coffee',
-      photo: `assets/images/irish_coffee.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 12,
-      name: 'Viennese coffee',
-      photo: `assets/images/viennese_coffee.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 13,
-      name: 'Frappé',
-      photo: `assets/images/frappe.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 14,
-      name: 'Azteca',
-      photo: `assets/images/azteca.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 15,
-      name: 'Arabica coffee',
-      photo: `assets/images/arabica.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 16,
-      name: 'Robusta coffee',
-      photo: `assets/images/robusta.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-    {
-      id: 17,
-      name: 'Combinations',
-      photo: `assets/images/combinations.jpg`,
-      availableUnits: 99,
-      place: 'Test place',
-      state: 'ST'
-    },
-  ];
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
