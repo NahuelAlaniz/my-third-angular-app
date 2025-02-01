ac9088d02538274bc885b99359d5a3720336866e
NahuelAlaniz <nahuelalaniz80@gmail.com>
2025-02-01 16:22:09 -0300

    Se añadio un formulario a la aplicacion web

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -153,4 +153,9 @@ export class HousingService {
   getHousingLocationById(id:number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+  submitApplication(firstName: string, lastName:string, email:string) {
+    console.log (
+      `Coffe application received: firstName:${firstName}, LastName:${lastName},email:${email}.`,
+    );
+  }
 }
